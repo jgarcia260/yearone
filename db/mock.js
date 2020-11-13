28c03af1e364e8b67126d439dd3518260f1a2343
Jorge Garcia <jorgarcia1994@gmail.com>
2020-11-12 18:19:28 -0800

    served static files and movies endpoint for data

---
+++ db/mock.js
@@ -0,0 +1,50 @@
+// Title
+// Director
+// Release Year
+// Description(if your API provides it)
+// Ability to thumbs up or thumbs down the movie(see below)
+
+let movies = [
+  {
+    'title': 'movie1',
+    'director': 'me',
+    'release_year': '2020-11-14',
+    'description': 'movie1 desc',
+    'upvotes': 0,
+    'downvotes': 0,
+  },
+  {
+    'title': 'movie2',
+    'director': 'me',
+    'release_year': '2020-11-14',
+    'description': 'movie2 desc',
+    'upvotes': 0,
+    'downvotes': 0,
+  },
+  {
+    'title': 'movie3',
+    'director': 'me',
+    'release_year': '2020-11-14',
+    'description': 'movie3 desc',
+    'upvotes': 0,
+    'downvotes': 0,
+  },
+  {
+    'title': 'movie4',
+    'director': 'me',
+    'release_year': '2020-11-14',
+    'description': 'movie4 desc',
+    'upvotes': 0,
+    'downvotes': 0,
+  },
+  {
+    'title': 'movie5',
+    'director': 'me',
+    'release_year': '2020-11-14',
+    'description': 'movie5 desc',
+    'upvotes': 0,
+    'downvotes': 0,
+  },
+];
+
+module.exports = movies;
--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const request = require('supertest');
 const app = express();
+const data = require('../db/mock.js');
 const PORT = 7999;
 
 app.use(express.json());
+app.use('/', express.static('../public'));
 
-app.get('/:user', (req, res) => {
-  res.status(200).json('hello world');
+app.get('/movies', (req, res) => {
+  res.status(200).json(data);
 });
 
-request(app)
-  .get('/:user')
-  .expect('Content-Type', /json/)
-  .expect('Content-Length', '13')
-  .expect(200)
-  .end((err, res) => {
-    if (err) throw err;
-  });
+// app.get('/:user', (req, res) => {
+//   res.status(200).json('hello world');
+// });
+
+// request(app)
+//   .get('/:user')
+//   .expect('Content-Type', /json/)
+//   .expect('Content-Length', '13')
+//   .expect(200)
+//   .end((err, res) => {
+//     if (err) throw err;
+//   });
 
 app.listen(PORT, () => {
   console.log(`connected to http://localhost:${PORT}`);
