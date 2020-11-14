b512a37365fd3f9c978b5faf1a0e4f861885365a
Jorge Garcia <jorgarcia1994@gmail.com>
2020-11-13 22:23:24 -0800

    added functionality to searchbar

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -2,6 +2,32 @@ const express = require('express');
 const moviesController = require('../controllers/movies.js');
 const router = express.Router();
 
+router.get('/query/:query', (req, res) => {
+  console.log('query')
+  let { query } = req.params;
+  console.log(query)
+  if (query === '') {
+    return res.status(400);
+  }
+  moviesController.getMovie(query, (err, result) => {
+    if (err) {
+      return res.status(400);
+    }
+    return res.status(200).send(result);
+  })
+});
+
+router.get('/director/:movie_id', (req, res) => {
+  let { movie_id } = req.params;
+  console.log(movie_id, 'movieid')
+  moviesController.getMovieDirector(movie_id, (err, result) => {
+    if (err) {
+      return res.status(400);
+    }
+    return res.status(200).send(result);
+  })
+});
+
 router.get('/', (req, res) => {
   moviesController.getTrendingMovies((err, result) => {
     if (err) {
@@ -10,32 +36,5 @@ router.get('/', (req, res) => {
     return res.status(200).send(result);
   })
 });
-
-// router.get('/:movieTitle', () => {
-
-// });
-
-// {
-//   "id": 340102,
-//   "video": false,
-//   "vote_count": 576,
-//   "vote_average": 6.2,
-//   "title": "The New Mutants",
-//   "release_date": "2020-08-26",
-//   "original_language": "en",
-//   "original_title": "The New Mutants",
-//   "genre_ids": [
-//     28,
-//     878,
-//     27,
-//     12
-//   ],
-//   "backdrop_path": "/eCIvqa3QVCx6H09bdeOS8Al2Sqy.jpg",
-//   "adult": false,
-//   "overview": "Five young mutants, just discovering their abilities while held in a secret facility against their will, fight to escape their past sins and save themselves.",
-//   "poster_path": "/xrI4EnZWftpo1B7tTvlMUXVOikd.jpg",
-//   "popularity": 1195.685,
-//   "media_type": "movie"
-// },
 
 module.exports = router;
